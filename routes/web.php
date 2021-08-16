a912015441e1b52c2cb9ff6fcf4e2190619d528a
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-08-17 00:37:03 +0530

    1. How url loads a view 2. Include js & css 3. Link to different pages 4. Store blog post as HTML files 5. Route wildcard constraints 6. Use Caching for expensive operation

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('/posts/{post}', function ($slug) {
+
+
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
+        abort(404);
+        // return redirect('/');
+    }
+
+    // caching the post for 5 seconds
+    $post = cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
+    // // using normal function
+    //  function () use ($path) { var_dump('file_get_contents'); return  file_get_contents($path);}); // end using normal function
+
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_-]+'); // 'post' is the name of the wildcard and the second one is the regular expression (ROUTE WILDCARD CONSTRAINTS)
+    // ->whereAlpha('post');
+    // ->whereAlphaNumeric('post');
+    // ->whereNumber('post');
