0dbdfceed38b53f7d6789e4f28a513f7f2ff9679
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-08-17 22:12:15 +0530

    9. Collection sorting and caching refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -30,20 +30,23 @@ class Post
 
     public static function all()
     {
-        // find all the files in the post directory and collect them into a collection and loop over or map over each item and reach one, parse that file into the  $document once then we ve got the collection of document map over the second time and this time we gonna build up our own post object and pass it to view
-        return  collect($files = File::files(resource_path("posts/"))) //assign File:: files(resource_path...) in $files as inline
-            ->map(
-                fn ($file) => YamlFrontMatter::parseFile($file)
-            )
-            ->map(
-                fn ($document) =>  new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
+        return cache()->rememberForever('post.all', function () {
+            // find all the files in the post directory and collect them into a collection and loop over or map over each item and reach one, parse that file into the  $document once then we ve got the collection of document map over the second time and this time we gonna build up our own post object and pass it to view
+            return  collect($files = File::files(resource_path("posts/"))) //assign File:: files(resource_path...) in $files as inline
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(
+                    fn ($document) =>  new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug
+                    )
                 )
-            );
+                ->sortByDesc('date');
+        });
+
+
         // // return File::files(resource_path("posts/")); //return the whole files inside the posts folder
         // $files = File::files(resource_path("posts/"));
 
@@ -55,22 +58,24 @@ class Post
     }
 
 
-
-
     public static function find($slug)
     {
-        // if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
+        // // if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
 
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {  //same as above line but this time using resource path instead of __DIR__.
-            abort(404);
-            // throw new ModelNotFoundException();
-        }
-        // caching the post for 5 seconds
-        return cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
+        // if (!file_exists($path = resource_path("posts/{$slug}.html"))) {  //same as above line but this time using resource path instead of __DIR__.
+        //     abort(404);
+        //     // throw new ModelNotFoundException();
+        // }
+        // // caching the post for 5 seconds
+        // return cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
 
-        // $post = cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
-        // // // using normal function
-        // //  function () use ($path) { var_dump('file_get_contents'); return  file_get_contents($path);}); // end using normal function
-        // return view('post', ['post' => $post]);
+        // // $post = cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
+        // // // // using normal function
+        // // //  function () use ($path) { var_dump('file_get_contents'); return  file_get_contents($path);}); // end using normal function
+        // // return view('post', ['post' => $post]);
+
+        // of all the blog posts, find the one with a slug that matches the one that was requested.
+        return static::all()->firstWhere('slug', $slug); // fetch all from static all() function 
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,18 +12,10 @@
 
 <body>
     <article>
-        <?= $post ?>
-        {{-- {!! $post !!} --}}
-        {{-- <h1>
-            <a href="/post">My First Post</a>
-        </h1>
 
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-            leap into
-            electronic typesetting, remaining essentially unchanged.</p> --}}
+        <h1>{!! $post->title !!}</h1>
+
+        <div>{!! $post->body !!}</div>
     </article>
     <a href="/">Go Back</a>
 </body>
