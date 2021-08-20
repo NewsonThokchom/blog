5ab7784f99f5ddc3ca32f5916d24b40359a88671
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-08-20 18:05:32 +0530

    10. The Blade - Layout files and a few tweaks and consideration (added findOrfail function in Post Model).

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
-</head>
-
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article>
-
             <h1>
                 <a href="/posts/{{ $post->slug }}"> {!! $post->title !!} </a>
             </h1>
@@ -23,7 +10,4 @@
 
         </article>
     @endforeach
-
-</body>
-
-</html>
+</x-layout>
