dbfd0bd6fdb806d4b27dd31a911b69dfb2581db7
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-08-20 16:00:23 +0530

    9. The Basic Code cleanup

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -45,35 +45,11 @@ class Post
                 )
                 ->sortByDesc('date');
         });
-
-
-        // // return File::files(resource_path("posts/")); //return the whole files inside the posts folder
-        // $files = File::files(resource_path("posts/"));
-
-        // // return array_map(function ($file) {
-        // //     return $file->getContents();
-        // // }, $files);
-
-        // return array_map(fn ($file) => $file->getContents(), $files); // same as above but this use arrow function
     }
 
 
     public static function find($slug)
     {
-        // // if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-
-        // if (!file_exists($path = resource_path("posts/{$slug}.html"))) {  //same as above line but this time using resource path instead of __DIR__.
-        //     abort(404);
-        //     // throw new ModelNotFoundException();
-        // }
-        // // caching the post for 5 seconds
-        // return cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
-
-        // // $post = cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path)); //using arrow function
-        // // // // using normal function
-        // // //  function () use ($path) { var_dump('file_get_contents'); return  file_get_contents($path);}); // end using normal function
-        // // return view('post', ['post' => $post]);
-
         // of all the blog posts, find the one with a slug that matches the one that was requested.
         return static::all()->firstWhere('slug', $slug); // fetch all from static all() function 
 
