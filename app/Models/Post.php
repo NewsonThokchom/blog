da84ac9d8f656167652734fb383a083b3771de57
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-08-22 23:09:22 +0530

    13. add eager loading in post model instead of route function, added username field in user factory and migration, get post by author is added, solved n+1 problem using eagerloading

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,9 @@ class Post extends Model
 {
     use HasFactory;
     protected  $guarded = [];
+
+    protected $with = ['category', 'author']; // to add category and author data for every request of Post #if we dont want this category and author for specfic request we can call the Post data without them by using "without(['author', 'cetegory'])"  
+
     // protected  $fillable = ['title', 'excerpt', 'body'];
 
     // search by slug , another option to search in slug
@@ -22,5 +25,15 @@ class Post extends Model
     {
         // hasOne, hasMany, belongsTo, belongsToMany
         return  $this->belongsTo(Category::class);
+    }
+
+    // public function user()  ////foriegn_key will be 'user_id'
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
+
+    public function author()  ////foriegn_key will be 'author_id'
+    {
+        return $this->belongsTo(User::class, 'user_id'); // second argument will override foriegn_key i.e author_id will be user_id
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 /*
@@ -18,7 +19,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
     return view('posts', [
         // 'posts' => Post::all() ////Triggered N+1 problems
-        'posts' => Post::with('category')->get() // Eager loading can solve n+1 problem
+        // 'posts' => Post::latest()->with(['category', 'author'])->get() // Eager loading can solve n+1 problem
+        'posts' => Post::latest()->get() // change Eager loading "->with(['category', 'author'])" in Post Model 
     ]);
 });
 
@@ -34,5 +36,11 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
